2973720501577d9107b4e1eedba2c49e3ffab454
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-03-04 18:16:08 -0500

    var name changed and py version updated

--- test_moodDetection.py
+++ test_moodDetection.py
@@ -7,5 +7,5 @@ def test_moodDetection():
     """
     Returns True if the emotion detected in image is happy
     """
-    testImg = cv2.imread("images/nice-man-smiling.jpg")
-    assert md.detectEmotion(testImg) == "happy"
+    test_img = cv2.imread("images/nice-man-smiling.jpg")
+    assert md.detectEmotion(test_img) == "happy"

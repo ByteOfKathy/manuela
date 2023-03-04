5ab09087cb234a3acf31b13f778165fdce3f8705
Chris Bloodsworth <christopherbloodsworth@gmail.com>
2023-03-04 18:48:56 -0500

    Removed erroneous function

--- emotionTextDetection.py
+++ emotionTextDetection.py
@@ -2,19 +2,8 @@ import text2emotion as te
 
 def get_emotion_helper() -> dict:
     name = input("Hello! I'm Manuela. What's your name? \n")
-    return get_emotion(input("Hello " + name + ", how are you feeling today? \n"))
+    return te.get_emotion(input("Hello " + name + ", how are you feeling today? \n"))
 
-
-def get_emotion(text: str) -> dict:
-    """
-    Returns a dictionary with the emotion and its value as a percentage 0.0-1.0
-    :param text: the text to analyze
-    :return: a dictionary with the emotion and its value
-
-    Example:
-    >>> get_emotion("I am happy")
-    """
-    return te.get_emotion(text)
 
 def interpret_emotion(emotion: dict) -> dict:
     """

b1bf91833987fe335aac5cce7ccf901592e33080
Kathy C <katherinechan4@gmail.com>
2023-03-04 11:11:47 -0500

    docs: add docstring for detectSmile and rename

--- test_moodDetection.py
+++ test_moodDetection.py
@@ -4,5 +4,8 @@ import cv2
 
 
 def test_moodDetection():
+    """
+    Returns True if there is at least 1 smile detected in image
+    """
     testImg = cv2.imread("images/nice-man-smiling.jpg")
     assert md.detectSmile(testImg) is not None

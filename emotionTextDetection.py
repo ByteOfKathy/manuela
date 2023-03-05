931b527ea32b5ea66540345203433f3d306488dc
cbloodsworth <christopherbloodsworth@gmail.com>
2023-03-04 21:22:30 -0500

    Reformatted using black

--- emotionTextDetection.py
+++ emotionTextDetection.py
@@ -4,13 +4,36 @@ import text2emotion as te
 import textRecognizer as tr
 import numpy as np
 
-happyResponses = ["That's great! I'm happy for you!", "I'm glad you're feeling happy!", "I'm glad you're feeling good!"]
-sadResponses = ["Keep your chin up, it'll be alright!", "I'm sorry to hear that. That must be really upsetting. I'm here for you!", "I'm sorry about that. Everything will be alright!"]
-madResponses = ["I'm sorry to hear that. That must be frustrating. I'm here for you!", "I'm sorry about that. I hope you feel better soon!"]
-fearResponses = ["I can see how you feel worried. It's okay, I'm here for you!", "I'm sorry to hear that. I hope you feel better soon!", "I'm sorry about that. I hope you feel better soon!"]
-surpriseResponses = ["Oh, that was unexpected!", "Wow, that's surprising!", "I'm surprised by that!"]
-disgustResponses = ["I'm sorry to hear that. That must have been icky. I'm here for you!", "I'm sorry about that. I hope you feel better soon!"]
+happyResponses = [
+    "That's great! I'm happy for you!",
+    "I'm glad you're feeling happy!",
+    "I'm glad you're feeling good!",
+]
+sadResponses = [
+    "Keep your chin up, it'll be alright!",
+    "I'm sorry to hear that. That must be really upsetting. I'm here for you!",
+    "I'm sorry about that. Everything will be alright!",
+]
+madResponses = [
+    "I'm sorry to hear that. That must be frustrating. I'm here for you!",
+    "I'm sorry about that. I hope you feel better soon!",
+]
+fearResponses = [
+    "I can see how you feel worried. It's okay, I'm here for you!",
+    "I'm sorry to hear that. I hope you feel better soon!",
+    "I'm sorry about that. I hope you feel better soon!",
+]
+surpriseResponses = [
+    "Oh, that was unexpected!",
+    "Wow, that's surprising!",
+    "I'm surprised by that!",
+]
+disgustResponses = [
+    "I'm sorry to hear that. That must have been icky. I'm here for you!",
+    "I'm sorry about that. I hope you feel better soon!",
+]
 neutralResponses = ["Ah, it seems it's a so-so day for you. That's okay!"]
+
 
 def get_emotion_helper() -> dict:
     tr.tts("Hello! I'm Manuela. What's your name? \n")
@@ -38,30 +61,40 @@ def respond_to_emotion(emotion: str):
 
     """
 
-    tr.tts(f"It seems you're mostly feeling {emotion}. Is that correct? Please respond 'yes' or 'no' \n")
+    tr.tts(
+        f"It seems you're mostly feeling {emotion}. Is that correct? Please respond 'yes' or 'no' \n"
+    )
 
     correct_response = input()
 
-     
     if correct_response.lower() == "yes":
-        if emotion == "Happy": tr.tts(random.choice(happyResponses))
-        elif emotion == "Sad": tr.tts(random.choice(sadResponses))
-        elif emotion == "Angry": tr.tts(random.choice(madResponses))
-        elif emotion == "Fear": tr.tts(random.choice(fearResponses))
-        elif emotion == "Surprise": tr.tts(random.choice(surpriseResponses))
-        elif emotion == "Disgust": tr.tts(random.choice(disgustResponses))
-        elif emotion == "Neutral": tr.tts(random.choice(neutralResponses))
+        if emotion == "Happy":
+            tr.tts(random.choice(happyResponses))
+        elif emotion == "Sad":
+            tr.tts(random.choice(sadResponses))
+        elif emotion == "Angry":
+            tr.tts(random.choice(madResponses))
+        elif emotion == "Fear":
+            tr.tts(random.choice(fearResponses))
+        elif emotion == "Surprise":
+            tr.tts(random.choice(surpriseResponses))
+        elif emotion == "Disgust":
+            tr.tts(random.choice(disgustResponses))
+        elif emotion == "Neutral":
+            tr.tts(random.choice(neutralResponses))
         else:
-            tr.tts("I'm sorry, I don't understand that emotion. Could you explain further? \n")
+            tr.tts(
+                "I'm sorry, I don't understand that emotion. Could you explain further? \n"
+            )
             respond_to_emotion(interpret_emotion(te.get_emotion(input())))
     elif correct_response.lower() == "no":
         tr.tts("I'm sorry, I must have misunderstood. Could you tell me more?")
         respond_to_emotion(interpret_emotion(te.get_emotion(input())))
 
 
-def get_strongest_emotion(t_emotions : dict, i_emotions : dict) -> str:
+def get_strongest_emotion(t_emotions: dict, i_emotions: dict) -> str:
     norm_emotions = dict()
-    for(t_emotion, t_value) in t_emotions.items():
+    for t_emotion, t_value in t_emotions.items():
         norm_emotions[t_emotion] = (t_value + i_emotions[t_emotion]) / 2
     return interpret_emotion(norm_emotions)
 

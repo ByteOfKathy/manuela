035ac4155425c9e01c768dfef25da0b97c35d64c
levyjonathan31 <jonathanlevy2002@gmail.com>
2023-03-05 01:15:32 -0500

    Bare bones video to emotion functionality

---
+++ videoDetection.py
@@ -0,0 +1,19 @@
+import time
+import cv2
+import moodDetection as md
+def startVideo():
+    """
+    Starts video capture
+    """
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    cap.release()
+    return frame
+def getEmotion() -> str:
+    """
+    Gets emotion from video
+    """
+    frame = startVideo()
+    emotion = md.detectEmotion(frame)
+    return emotion
+
